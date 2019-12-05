7f13351f950999576a3efc4ecb8ec2fef94a7230
Aurelius Wendelken <webtobesocial@gmail.com>
2019-12-05 03:46:02 +0100

    updated queries and return host as lowercase

--- asn_lookup.py
+++ asn_lookup.py
@@ -15,22 +15,22 @@ def connect():
     return MongoClient('mongodb://127.0.0.1:27017')
 
 
-def fetch_all(col):
-    return col.ipv4.find({'as': {'$exists': False}}, {'_id': 0}).sort([('_id', -1)])
+def fetch_all(db):
+    return db.ipv4.find({'as': {'$exists': False}}, {'_id': 0}).sort([('_id', -1)])
 
 
 def main():
-    db = connect()
-    col = db.ip_data
+    client = connect()
+    db = client.ip_data
 
-    for i in fetch_all(col):
+    for i in fetch_all(db):
         try:
-            host = socket.gethostbyaddr(i['ip'])[0]
+            host = socket.gethostbyaddr(i['ip'])[0].lower()
         except Exception:
             host = None
 
         res = asn_lookup(i['ip'])
-        col.ipv4.update_one({'ip': i['ip']}, {'$set': {'host': host, 'updated': datetime.utcnow()}, '$push': { 'as': res }}, upsert=False)
+        db.ipv4.update_one({'ip': i['ip']}, {'$set': {'host': host, 'updated': datetime.utcnow()}, '$push': {'as': res}}, upsert=False)
 
         print(i['ip'], res, host)
 

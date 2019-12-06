3c4aef9cf4b6194e4b29483ec9da82c9208030c5
Aurelius Wendelken <webtobesocial@gmail.com>
2019-12-06 18:06:57 +0100

    catching ipwhois exceptions error

--- extract_whois.py
+++ extract_whois.py
@@ -2,6 +2,7 @@
 
 from ipwhois.net import Net
 from ipwhois.asn import ASNOrigin
+from ipwhois.exceptions import ASNOriginLookupError
 
 from pymongo import MongoClient
 from pymongo.errors import DuplicateKeyError
@@ -20,7 +21,7 @@ def retrieve_asns(db):
 def update_data(db, asn, post):
     try:
         db.asn.update_one({'asn': asn}, {'$set': post}, upsert=False)
-        print(u'INFO: updated asn AS{} document'.format(asn))
+        print(u'INFO: updated whois AS{} document'.format(asn))
     except DuplicateKeyError:
         pass
 
@@ -32,8 +33,11 @@ if __name__ == '__main__':
     for asn in retrieve_asns(db):
         now = datetime.utcnow()
 
-        whois = ASNOrigin(net=Net(asn['ip'])).lookup(
-                asn=str(asn['asn']), retry_count=10, asn_methods=['whois'])
+        try:
+            whois = ASNOrigin(net=Net(asn['ip'])).lookup(
+                    asn=str(asn['asn']), retry_count=10, asn_methods=['whois'])
+        except ASNOriginLookupError:
+            whois = None
 
         if whois and len(whois['nets']) > 0:
             update_data(db, str(asn['asn']), {'updated': now, 'whois': whois['nets']})

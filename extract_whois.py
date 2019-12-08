82f58006ecd1c26074b798b72a54ef48c089093f
Aurelius Wendelken <webtobesocial@gmail.com>
2019-12-08 11:15:22 +0100

    refactored whois crawler due to major changes in db

--- extract_whois.py
+++ extract_whois.py
@@ -1,5 +1,7 @@
 #!/usr/bin/env python3
 
+import argparse
+
 from ipwhois.net import Net
 from ipwhois.asn import ASNOrigin, IPASN
 from ipwhois.exceptions import ASNOriginLookupError
@@ -15,43 +17,87 @@ def connect():
     return MongoClient('mongodb://127.0.0.1:27017')
 
 
+def retrieve_dns(db):
+    return db.dns.find({'whois': {'$exists': False},
+                        'a_record.0': {'$exists': True}})
+
+
 def retrieve_asns(db):
     return db.lookup.find({'whois': {'$exists': False}})
 
 
-def update_data(db, asn, post):
+def update_data_dns(db, ip, domain, post):
     try:
-        db.lookup.update_one({'asn': asn}, {'$set': post, '$unset': {'subnet': 0}}, upsert=False)
-        print(u'INFO: updated whois AS{} document'.format(asn))
+        db.lookup.update_one({'a_record': {'$in': [ip]}}, {'$set': post}, upsert=False)
+        print(u'INFO: updated dns whois entry for IP {}'.format(domain))
     except (DocumentTooLarge, DuplicateKeyError):
         pass
 
 
+def update_data_lookup(db, asn, post):
+    try:
+        db.lookup.update_one({'asn': asn}, {'$set': post, '$unset': {
+            'subnet': 0}}, upsert=False)
+        print(u'INFO: updated dns whois entry for AS{}'.format(asn))
+    except (DocumentTooLarge, DuplicateKeyError):
+        pass
+
+
+def get_whois(ip):
+    try:
+        whois = IPASN(Net(ip)).lookup(retry_count=10, asn_methods=['whois'])
+    except Exception:
+        whois = None
+
+    return whois
+
+
+def get_cidr(ip, asn):
+    try:
+        cidr = []
+
+        cidr = ASNOrigin(Net(ip)).lookup(
+            asn=str(asn), retry_count=10, asn_methods=['whois'])
+
+        if cidr and len(cidr['nets']) > 0:
+            for c in cidr['nets']:
+                cidr.append(c['cidr'])
+
+        print(cidr)
+    except ASNOriginLookupError:
+        cidr = None
+
+
+def argparser():
+    parser = argparse.ArgumentParser()
+    parser.add_argument('--collection', '-c', help='set collection to update')
+    args = parser.parse_args()
+
+    return args
+
+
 if __name__ == '__main__':
+    args = argparser()
+    print(args)
+
     client = connect()
     db = client.ip_data
+    now = datetime.utcnow()
 
-    for asn in retrieve_asns(db):
-        now = datetime.utcnow()
+    if args.collection == 'lookup':
+        for asn in retrieve_asns(db):
+            whois = get_whois(asn['ip'])
+            cidr = get_cidr(asn['ip'], asn['asn'])
 
-        try:
-            whois = IPASN(Net(asn['ip'])).lookup(retry_count=10, asn_methods=['whois'])
-        except Exception:
-            whois = None
+            if whois and cidr and len(whois) > 0:
+                update_data_lookup(db, asn['asn'], {'updated': now,
+                                                    'cidr': cidr,
+                                                    'whois': whois})
+    elif args.collection == 'dns':
+        for dns in retrieve_dns(db):
+            whois = get_whois(dns['a_record'][0])
 
-        try:
-            cidr_list = []
-
-            cidr = ASNOrigin(Net(asn['ip'])).lookup(
-                    asn=str(asn['asn']), retry_count=10, asn_methods=['whois'])
-
-            if cidr and len(cidr['nets']) > 0:
-                for c in cidr['nets']:
-                    cidr_list.append(c['cidr'])
-
-            print(cidr_list)
-        except ASNOriginLookupError:
-            cidr_list = None
-
-        if whois and len(whois) > 0:
-            update_data(db, asn['asn'], {'updated': now, 'cidr': cidr_list, 'whois': whois})
+            if whois and len(whois) > 0:
+                update_data_dns(db, dns['a_record'][0], dns['domain'], {
+                                                            'updated': now,
+                                                            'whois': whois})

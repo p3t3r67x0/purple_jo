867112a19ec01e808ad6e70b40300aed47885583
Aurelius Wendelken <webtobesocial@gmail.com>
2019-12-10 08:20:20 +0100

    fixed broken statement query

--- api.py
+++ api.py
@@ -99,7 +99,7 @@ def fetch_all_prefix(prefix):
 
 
 def fetch_all_asn(asn):
-    return mongo.lookup.dns.find({'whois.asn': asn}, {'_id': 0}).limit(5)
+    return mongo.db.lookup.find({'whois.asn': asn}, {'_id': 0}).limit(5)
 
 
 def fetch_all_dns(domain):

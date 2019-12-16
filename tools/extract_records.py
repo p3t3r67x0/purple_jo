d3dd970718d07106396b80f9553c4ec3e793e8e0
Aurelius Wendelken <webtobesocial@gmail.com>
2019-12-16 21:43:45 +0100

    added required commandline arguments host and worker

--- tools/extract_records.py
+++ tools/extract_records.py
@@ -1,6 +1,7 @@
 #!/usr/bin/env python3
 
 import multiprocessing
+import argparse
 
 from dns import resolver
 from dns.name import EmptyLabel
@@ -16,8 +17,8 @@ from pymongo import MongoClient
 from pymongo.errors import DuplicateKeyError
 
 
-def connect():
-    return MongoClient('mongodb://127.0.0.1:27017')
+def connect(host):
+    return MongoClient('mongodb://{}:27017'.format(host))
 
 
 def update_data(db, item, post):
@@ -57,8 +58,8 @@ def get_address(domain, record):
         return
 
 
-def worker(skip, limit):
-    client = connect()
+def worker(host, skip, limit):
+    client = connect(args.host)
     db = client.ip_data
 
     domains = retrieve_domains(db, limit, skip)
@@ -95,17 +96,27 @@ def worker(skip, limit):
     return
 
 
+def argparser():
+    parser = argparse.ArgumentParser()
+    parser.add_argument('--worker', help='set worker count', type=int, required=True)
+    parser.add_argument('--host', help='set the host', type=str, required=True)
+    args = parser.parse_args()
+
+    return args
+
+
 if __name__ == '__main__':
-    client = connect()
+    args = argparser()
+    client = connect(args.host)
     db = client.ip_data
 
     jobs = []
-    threads = 32
-    amount = db.dns.estimated_document_count() / threads
+    threads = args.worker
+    amount = round(db.dns.estimated_document_count() / (threads + 50000))
     limit = amount
 
     for f in range(threads):
-        j = multiprocessing.Process(target=worker, args=(limit, amount))
+        j = multiprocessing.Process(target=worker, args=(args.host, limit, amount))
         jobs.append(j)
         j.start()
         limit = limit + amount

22474b19e15d5651e47158598f2d34f604005a96
Aurelius Wendelken <webtobesocial@gmail.com>
2019-12-16 22:13:23 +0100

    added required commandline argument host and input

--- tools/import_ip.py
+++ tools/import_ip.py
@@ -1,6 +1,6 @@
 #!/usr/bin/env python3
 
-import sys
+import argparse
 
 from pymongo import MongoClient
 from pymongo.errors import DuplicateKeyError
@@ -11,16 +11,26 @@ def load(i):
         return f.readlines()
 
 
-def connect():
-    return MongoClient('mongodb://127.0.0.1:27017')
+def connect(host):
+    return MongoClient('mongodb://{}:27017'.format(host))
+
+
+def argparser():
+    parser = argparse.ArgumentParser()
+    parser.add_argument('--input', help='set input file name', type=str, required=True)
+    parser.add_argument('--host', help='set the host', type=str, required=True)
+    args = parser.parse_args()
+
+    return args
 
 
 def main():
-    client = connect()
+    args = argparser()
+    client = connect(args.host)
     db = client.ip_data
     db.ipv4.create_index('ip', unique=True)
 
-    for line in load(sys.argv[1]):
+    for line in load(args.input):
         try:
             db.ipv4.insert_one({'ip': line.strip()})
             print(line.strip())

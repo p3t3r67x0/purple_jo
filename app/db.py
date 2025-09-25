8181535ca793212a7b393bc583259789b3ed3248
Aurelius Wendelken <webtobesocial@gmail.com>
2025-09-25 11:31:56 +0200

    expanded the mongo db index coverage

--- app/db.py
+++ app/db.py
@@ -48,12 +48,195 @@ TEXT_INDEX_NAME = "all_fields_text_index"
 DOMAIN_UNIQUE_INDEX = IndexModel(
     [("domain", 1)],
     name="domain_unique_index",
+    sparse=True,
     unique=True,
+    background=True,
 )
 
 A_RECORD_SPARSE_INDEX = IndexModel(
-    [("a_record", 1)],
-    name="a_record_sparse_index",
+    [("a_record", 1), ("updated", -1)],
+    name="a_record_updated_index",
+    sparse=True,
+    background=True,
+)
+
+AAAA_RECORD_SPARSE_INDEX = IndexModel(
+    [("aaaa_record", 1), ("updated", -1)],
+    name="aaaa_record_updated_index",
+    sparse=True,
+    background=True,
+)
+
+UPDATED_DESC_INDEX = IndexModel(
+    [("updated", -1)],
+    name="updated_desc_index",
+    background=True
+)
+
+WHOIS_ASN_INDEX = IndexModel(
+    [("whois.asn", 1), ("updated", -1)],
+    name="whois_asn_updated_index",
+    sparse=True,
+    background=True,
+)
+
+WHOIS_ASN_CIDR_INDEX = IndexModel(
+    [("whois.asn_cidr", 1), ("updated", -1)],
+    name="whois_asn_cidr_updated_index",
+    sparse=True,
+    background=True,
+)
+
+WHOIS_ASN_REGISTRY_INDEX = IndexModel(
+    [("whois.asn_registry", 1), ("updated", -1)],
+    name="whois_asn_registry_updated_index",
+    sparse=True,
+    background=True,
+)
+
+WHOIS_ASN_COUNTRY_INDEX = IndexModel(
+    [("whois.asn_country_code", 1), ("updated", -1)],
+    name="whois_asn_country_updated_index",
+    sparse=True,
+    background=True,
+)
+
+GEO_COUNTRY_CODE_INDEX = IndexModel(
+    [("geo.country_code", 1), ("updated", -1)],
+    name="geo_country_code_updated_index",
+    sparse=True,
+    background=True,
+)
+
+GEO_STATE_INDEX = IndexModel(
+    [("geo.state", 1), ("updated", -1)],
+    name="geo_state_updated_index",
+    sparse=True,
+    background=True,
+)
+
+GEO_CITY_INDEX = IndexModel(
+    [("geo.city", 1), ("updated", -1)],
+    name="geo_city_updated_index",
+    sparse=True,
+    background=True,
+)
+
+PORTS_PORT_INDEX = IndexModel(
+    [("ports.port", 1), ("updated", -1)],
+    name="ports_port_updated_index",
+    sparse=True,
+    background=True,
+)
+
+MX_EXCHANGE_INDEX = IndexModel(
+    [("mx_record.exchange", 1), ("updated", -1)],
+    name="mx_exchange_updated_index",
+    sparse=True,
+    background=True,
+)
+
+NS_RECORD_INDEX = IndexModel(
+    [("ns_record", 1), ("updated", -1)],
+    name="ns_record_updated_index",
+    sparse=True,
+    background=True,
+)
+
+CNAME_TARGET_INDEX = IndexModel(
+    [("cname_record.target", 1), ("updated", -1)],
+    name="cname_target_updated_index",
+    sparse=True,
+    background=True,
+)
+
+HEADER_STATUS_INDEX = IndexModel(
+    [("header.status", 1), ("updated", -1)],
+    name="header_status_updated_index",
+    sparse=True,
+    background=True,
+)
+
+HEADER_POWERED_BY_INDEX = IndexModel(
+    [("header.x-powered-by", 1), ("updated", -1)],
+    name="header_powered_by_updated_index",
+    sparse=True,
+    background=True,
+)
+
+SSL_SUBJECT_CN_INDEX = IndexModel(
+    [("ssl.subject.common_name", 1), ("updated", -1)],
+    name="ssl_subject_common_name_index",
+    sparse=True,
+    background=True,
+)
+
+SSL_SUBJECT_ALT_NAMES_INDEX = IndexModel(
+    [("ssl.subject_alt_names", 1), ("updated", -1)],
+    name="ssl_subject_alt_names_index",
+    sparse=True,
+    background=True,
+)
+
+SSL_ISSUER_ORG_INDEX = IndexModel(
+    [("ssl.issuer.organization_name", 1), ("updated", -1)],
+    name="ssl_issuer_org_index",
+    sparse=True,
+    background=True,
+)
+
+SSL_ISSUER_CN_INDEX = IndexModel(
+    [("ssl.issuer.common_name", 1), ("updated", -1)],
+    name="ssl_issuer_common_name_index",
+    sparse=True,
+    background=True,
+)
+
+SSL_SUBJECT_OU_INDEX = IndexModel(
+    [("ssl.subject.organizational_unit_name", 1), ("updated", -1)],
+    name="ssl_subject_ou_index",
+    sparse=True,
+    background=True,
+)
+
+SSL_ISSUER_OU_INDEX = IndexModel(
+    [("ssl.issuer.organizational_unit_name", 1), ("updated", -1)],
+    name="ssl_issuer_ou_index",
+    sparse=True,
+    background=True,
+)
+
+SSL_CA_ISSUERS_INDEX = IndexModel(
+    [("ssl.ca_issuers", 1), ("updated", -1)],
+    name="ssl_ca_issuers_index",
+    sparse=True,
+    background=True,
+)
+
+SSL_OCSP_INDEX = IndexModel(
+    [("ssl.ocsp", 1), ("updated", -1)],
+    name="ssl_ocsp_index",
+    sparse=True,
+    background=True,
+)
+
+SSL_CRL_INDEX = IndexModel(
+    [("ssl.crl_distribution_points", 1), ("updated", -1)],
+    name="ssl_crl_distribution_points_index",
+    sparse=True,
+    background=True,
+)
+
+SSL_NOT_BEFORE_INDEX = IndexModel(
+    [("ssl.not_before", -1)],
+    name="ssl_not_before_index",
+    sparse=True,
+    background=True,
+)
+
+SSL_NOT_AFTER_INDEX = IndexModel(
+    [("ssl.not_after", -1)],
+    name="ssl_not_after_index",
     sparse=True,
     background=True,
 )
@@ -80,7 +263,33 @@ INDEXES = [
     DOMAIN_UNIQUE_INDEX,
     TEXT_INDEX_DEFINITION,
     A_RECORD_SPARSE_INDEX,
+    AAAA_RECORD_SPARSE_INDEX,
     GEO_LOC_INDEX,
+    UPDATED_DESC_INDEX,
+    WHOIS_ASN_INDEX,
+    WHOIS_ASN_CIDR_INDEX,
+    WHOIS_ASN_REGISTRY_INDEX,
+    WHOIS_ASN_COUNTRY_INDEX,
+    GEO_COUNTRY_CODE_INDEX,
+    GEO_STATE_INDEX,
+    GEO_CITY_INDEX,
+    PORTS_PORT_INDEX,
+    MX_EXCHANGE_INDEX,
+    NS_RECORD_INDEX,
+    CNAME_TARGET_INDEX,
+    HEADER_STATUS_INDEX,
+    HEADER_POWERED_BY_INDEX,
+    SSL_SUBJECT_CN_INDEX,
+    SSL_SUBJECT_ALT_NAMES_INDEX,
+    SSL_ISSUER_ORG_INDEX,
+    SSL_ISSUER_CN_INDEX,
+    SSL_SUBJECT_OU_INDEX,
+    SSL_ISSUER_OU_INDEX,
+    SSL_CA_ISSUERS_INDEX,
+    SSL_OCSP_INDEX,
+    SSL_CRL_INDEX,
+    SSL_NOT_BEFORE_INDEX,
+    SSL_NOT_AFTER_INDEX,
 ]
 
 
@@ -107,7 +316,8 @@ def _normalize_weights(weights: Any) -> Any:
 
 
 def _normalize_index_document(doc: Mapping[str, Any]) -> dict[str, Any]:
-    ignored = {"ns", "v", "background", "textIndexVersion", "2dsphereIndexVersion"}
+    ignored = {"ns", "v", "background",
+               "textIndexVersion", "2dsphereIndexVersion"}
     normalized: dict[str, Any] = {}
     for key, value in doc.items():
         if key in ignored:
